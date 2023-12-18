70bdc2ffa6f92746470c5d9d988b8e540164db98
Keagan <keygun2k1@gmail.com>
2023-12-18 14:31:03 +0100

    Installable and useable menu dropdown component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './styles/main.scss'
 import App from './App.vue'
+import MenuDropdown from './components/MenuDropdown.vue'
 
 createApp(App).mount('#app')
+
+export function BootVueComponents() {
+    createApp({})
+        .component('menu-dropdown', MenuDropdown)
+        .mount('#menu')
+}
